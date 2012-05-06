e610627e45a49de1c9936d2f23600634452f3e93
Binh Tran <binh_tran@web.de>
2012-05-06 20:41:57 +0200

    XCode Projekt hinzugefügt

---
+++ Poker/Classes/PokerAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PokerAppDelegate.h
+//  Poker
+//
+//  Created by Binh Tran on 05.05.12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PokerAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
