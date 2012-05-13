8fd8458a170d89e87d0fd6c2639f2dcf3950bcaf
Christian Klumpp <christian-klumpp@gmx.de>
2012-05-13 21:35:51 +0200

    einige User-Interface-Veränderungen

--- Poker iPhone App/Poker iPhone App/CustomTableViewCell.m
+++ Poker iPhone App/Poker iPhone App/CustomTableViewCell.m
@@ -10,6 +10,8 @@
 
 @implementation CustomTableViewCell
 
+@synthesize userImageImageView;
+
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
