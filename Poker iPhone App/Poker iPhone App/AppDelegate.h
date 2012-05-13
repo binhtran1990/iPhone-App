364c6e1fce9a418624387fa6694c764a7beef4f5
Binh Tran <binh_tran@freenet.de>
2012-05-13 18:37:04 +0200

    neues Projekt

---
+++ Poker iPhone App/Poker iPhone App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Poker iPhone App
+//
+//  Created by Lion User on 13/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Poker iPhone App/Poker iPhone App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Poker iPhone App
+//
+//  Created by Lion User on 13/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
